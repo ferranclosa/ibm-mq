08c36bcedb281856edc82bd77b0a1dcb77e435c6
Ferran Closa Cervera <36318284+ferranclosa@users.noreply.github.com>
2022-07-06 21:41:30 +0200

    First Commit

---
+++ src/test/java/com/closa/jmsmqibm/JmsMqIbmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.closa.jmsmqibm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmsMqIbmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
